dae42a4e9d1e439f11052cdf76268e7feb739bb4
Wang Xin <xeranic@gmail.com>
2014-08-12 22:45:17 -0700

    implement EventSource and EventBus

--- src/main/java/com/github/xeranic/blacksmith/event/EventBus.java
+++ src/main/java/com/github/xeranic/blacksmith/event/EventBus.java
@@ -1,23 +1,73 @@
 package com.github.xeranic.blacksmith.event;
 
-public final class EventBus {
+import java.util.HashMap;
+import java.util.Map;
 
-	public <L> ListenerRegistration<L> addListener(Class<Event<L>> eventClass, L listener) {
-		return null;
-	}
-	
-	public <L> boolean removeListener(Class<Event<L>> eventClass, L listener) {
-		return false;
-	}
-	
-	public void removeAllListeners() {
-		
-	}
-	
-	public void fire(Event<?> e) {
-		
-	}
-	
-	
-	
+public class EventBus {
+
+    private final Map<Class<?>, EventSource<?, ?>> eventSources = new HashMap<>();
+
+    public final <E extends Event<L>, L> ListenerRegistration<L> addListener(
+            Class<E> eventClass, L listener) {
+        return addListener(eventClass, listener, false);
+    }
+
+    public final <E extends Event<L>, L> ListenerRegistration<L> addListener(
+            Class<E> eventClass, L listener, boolean once) {
+        return addListener(eventClass, listener, once,
+                EventSource.PRIORITY_NORMAL);
+    }
+
+    public final <E extends Event<L>, L> ListenerRegistration<L> addListener(
+            Class<E> eventClass, L listener, boolean once, int priority) {
+        EventSource<E, L> eventSource = getEventSource(eventClass, true);
+        return eventSource.addListener(listener, once, priority);
+    }
+    
+    public final <E extends Event<L>, L> ListenerRegistration<L> on(Class<E> eventClass, L listener) {
+        return addListener(eventClass, listener);
+    }
+    
+    public final <E extends Event<L>, L> ListenerRegistration<L> once(Class<E> eventClass, L listener) {
+        return addListener(eventClass, listener, true);
+    }
+
+    public final <E extends Event<L>, L> boolean removeListener(Class<E> eventClass, L listener) {
+        EventSource<E, L> eventSource = getEventSource(eventClass, false);
+        if (eventSource == null) {
+            return false;
+        }
+        return eventSource.removeListener(listener);
+    }
+    
+    public final synchronized void removeAllListeners() {
+        for (EventSource<?, ?> eventSource : eventSources.values()) {
+            eventSource.removeAllListeners();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public final <E extends Event<L>, L> void fire(E e) {
+        EventSource<E, L> eventSource = getEventSource(e.getClass());
+        if (eventSource != null) {
+            eventSource.fire(e);
+        }
+    }
+
+    public final <E extends Event<L>, L> EventSource<E, L> getEventSource(
+            Class<E> eventClass) {
+        return getEventSource(eventClass, false);
+    }
+
+    @SuppressWarnings("unchecked")
+    private synchronized <E extends Event<L>, L> EventSource<E, L> getEventSource(
+            Class<E> eventClass, boolean create) {
+        EventSource<E, L> eventSource = (EventSource<E, L>) eventSources.get(eventClass);
+        if (eventSource == null && create) {
+            eventSource = new EventSource<>();
+            eventSources.put(eventClass, eventSource);
+        }
+        return eventSource;
+    }
+
 }
--- src/main/java/com/github/xeranic/blacksmith/event/EventSource.java
+++ src/main/java/com/github/xeranic/blacksmith/event/EventSource.java
@@ -1,40 +1,94 @@
 package com.github.xeranic.blacksmith.event;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public final class EventSource<E extends Event<L>, L> {
 
-	public static final int DEFAULT_PRIORITY = 100;
+    public static final int PRIORITY_LOWEST = 100;
+    
+    public static final int PRIORITY_LOWER = 200;
+    
+    public static final int PRIORITY_LOW = 300;
+    
+    public static final int PRIORITY_NORMAL = 400;
+    
+    public static final int PRIORITY_HIGH = 500;
+    
+    public static final int PRIORITY_HIGHER = 600;
+    
+    public static final int PRIORITY_HIGHEST = 700;
 
-	public ListenerRegistration<L> addListener(L listener) {
-		return addListener(listener, false);
-	}
+    private static class Item<L> {
+        private int priority;
+        private boolean once;
+        private L listener;
+    }
 
-	public ListenerRegistration<L> addListener(L listener, boolean once) {
-		return addListener(listener, once, DEFAULT_PRIORITY);
-	}
+    private List<Item<L>> items;
 
-	public ListenerRegistration<L> addListener(L listener, boolean once,
-			int priority) {
-		return null;
-	}
-	
-	public ListenerRegistration<L> on(L listener) {
-		return addListener(listener);
-	}
-	
-	public ListenerRegistration<L> once(L listener) {
-		return addListener(listener, true);
-	}
-	
-	public boolean removeListener(L listener) {
-		return false;
-	}
-	
-	public void removeAllListeners() {
-		
-	}
-	
-	public void fire(E event) {
-		
-	}
-	
+    public ListenerRegistration<L> addListener(L listener) {
+        return addListener(listener, false);
+    }
+
+    public ListenerRegistration<L> addListener(L listener, boolean once) {
+        return addListener(listener, once, PRIORITY_NORMAL);
+    }
+
+    public synchronized ListenerRegistration<L> addListener(L listener,
+            boolean once, int priority) {
+        if (items == null) {
+            items = new ArrayList<>();
+        }
+        int index = items.size();
+        for (int i = 0, len = items.size(); i < len; i++) {
+            Item<L> item = items.get(i);
+            if (item.priority > priority) {
+                index = i;
+                break;
+            }
+        }
+        Item<L> item = new Item<>();
+        item.priority = priority;
+        item.once = once;
+        item.listener = listener;
+        items.add(index, item);
+        return new ListenerRegistration<L>(this, listener);
+    }
+
+    public ListenerRegistration<L> on(L listener) {
+        return addListener(listener);
+    }
+
+    public ListenerRegistration<L> once(L listener) {
+        return addListener(listener, true);
+    }
+
+    public synchronized boolean removeListener(L listener) {
+        for (int i = 0, len = items.size(); i < len; i++) {
+            Item<L> item = items.get(i);
+            if (item.listener == listener) {
+                items.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public synchronized void removeAllListeners() {
+        items.clear();
+    }
+
+    public synchronized void fire(E event) {
+        Iterator<Item<L>> iter = items.iterator();
+        while (iter.hasNext()) {
+            Item<L> item = iter.next();
+            event.dispatchEvent(item.listener);
+            if (item.once) {
+                iter.remove();
+            }
+        }
+    }
+
 }
